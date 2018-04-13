f8f4fdfb62294301cd3a29be6c41888a47e846c0
theFellowCitizen <5im15rafarsinejad@gmx.ch>
2018-04-13 08:37:41 +0200

    small commit to test on Lucy's pc

--- Alumni/src/java/ch/kbw/control/adminController.java
+++ Alumni/src/java/ch/kbw/control/adminController.java
@@ -5,10 +5,36 @@
  */
 package ch.kbw.control;
 
+import java.awt.image.BufferedImage;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author anon
  */
+@Named
+@RequestScoped
 public class adminController {
+    
+    private BufferedImage bImage;
+    
+    public void uploadPicture(){
+        
+    }
+
+    /**
+     * @return the bImage
+     */
+    public BufferedImage getbImage() {
+        return bImage;
+    }
+
+    /**
+     * @param bImage the bImage to set
+     */
+    public void setbImage(BufferedImage bImage) {
+        this.bImage = bImage;
+    }
     
 }
