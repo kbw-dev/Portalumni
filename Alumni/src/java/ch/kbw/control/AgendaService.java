ee767058587fb6bbf8bdf2642a9be2d94a9264c0
Adel <Adel@DESKTOP-J9VBU9U>
2018-04-20 01:21:51 +0200

    task done -aaron 2018...

--- Alumni/src/java/ch/kbw/control/AgendaService.java
+++ Alumni/src/java/ch/kbw/control/AgendaService.java
@@ -9,10 +9,9 @@ import ch.kbw.dao.AgendaDAO;
 import ch.kbw.model.Post;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -24,17 +23,20 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class AgendaService implements Serializable {
+
     @Inject
     private AgendaDAO agendaDAO;
 
-    private FacesContext context;
+    private ActionMessagesController amc = new ActionMessagesController();
     private String title;
     private Date date;
     private String place;
     private String content;
     private String newContent;
     private String newTitle;
+    private String newPlace;
     
+
     public void updateList() {
         agendaDAO.listAllPosts();
     }
@@ -42,14 +44,15 @@ public class AgendaService implements Serializable {
     public void createNewPost() {
         java.sql.Date date1 = new java.sql.Date(this.date.getTime());
         agendaDAO.createPost(title, date1, place, content);
-        context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + title + " wurde erstellt."));
+        amc.showMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + title + " wurde erstellt.");
         this.title = "";
         this.place = "";
         this.content = "";
         this.date = null;
+        
         this.newContent = content;
         this.newTitle = title;
+        this.newPlace = place;
 
     }
 
@@ -59,16 +62,17 @@ public class AgendaService implements Serializable {
 
     public void deletePost(Post post) {
         agendaDAO.deleteByID(post);
-        context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + post.getTitle() + " wurde gelöscht."));
+        amc.showMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + post.getTitle() + " wurde gelöscht.");
     }
-    public void editPost(Post post){
+
+    public void editPost(Post post) {
+        
         newContent = post.getContent();
         newTitle = post.getTitle();
-        agendaDAO.makeChanges(post, newContent, newTitle);
+        newPlace = post.getPlace();
+        agendaDAO.makeChanges(post, newContent, newTitle, newPlace);
         updateList();
-        context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + post.getTitle() + " wurde bearbeitet."));
+        amc.showMessage("Aktion durchgeführt", "Beitrag mit dem Titel " + post.getTitle() + " wurde bearbeitet.");
     }
 
     public String getTitle() {
@@ -118,6 +122,12 @@ public class AgendaService implements Serializable {
     public void setNewTitle(String newTitle) {
         this.newTitle = newTitle;
     }
+
+    public void setNewPlace(String newPlace) {
+        this.newPlace = newPlace;
+    }
     
 
+   
+    
 }
